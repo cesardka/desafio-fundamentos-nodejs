80615002865c37e8e5f4196e1ad5b5654bff509c
Cesar H <cesardka@hotmail.com>
2020-09-05 02:59:16 -0300

    Resolvendo desafio Fundamentos NodeJS
    Implementadas as lógicas envolvidas para que possam ser feitas chamadas
    de adicionar e listar transações
    
    - Adição de transação
    
    Permitimos que envie transações dos tipos "income" e "outcome" para que
    seja definido se foi uma transação de saque ou depósito à conta
    
    Feita validação para impedir que realize um saque com valor acima do que
    tem disponível em conta (cheque especial aqui não!!)
    
    Feita validação para retornar saldo 0 caso tente listar valores de algum
    tipo de transação que ainda não exista

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,52 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balanceIncome = this.totalBalance("income");
+    const balanceOutcome = this.totalBalance("outcome");
+    const balanceTotal = this.currentBalance();
+
+    const transactionsBalance: Balance = {
+      income: balanceIncome,
+      outcome: balanceOutcome,
+      total: balanceTotal,
+    }
+
+    return transactionsBalance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const currentBalance = this.currentBalance();
+    const enoughFundsInBalance = currentBalance - value > 0 ? true : false;
+
+    if (type === "outcome" && !enoughFundsInBalance) {
+      throw new Error(`Not enough funds to withdraw ${value}`);
+    }
+
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+
+  private currentBalance(): number {
+    return this.totalBalance("income") - this.totalBalance("outcome");
+  }
+
+  private totalBalance(type: "income" | "outcome"): number {
+    const transactionsOfType = this.transactions.filter(transaction => transaction.type === type);
+    if (transactionsOfType.length < 1) {
+      return 0;
+    }
+
+    const transactionsValues = transactionsOfType.map(transaction => transaction.value);
+    const transactionsSum = transactionsValues.reduce((sum, transactionValue) => sum + transactionValue);
+
+    return transactionsSum;
   }
 }
 
